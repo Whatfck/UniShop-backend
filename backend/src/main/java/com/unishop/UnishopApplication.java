fd63b67092aa739825443e9157ea8f1aaa7dfeaa
Whatfck <danieleperez12@gmail.com>
2025-11-07 10:14:15 -0500

    fix docker build

---
+++ backend/src/main/java/com/unishop/UnishopApplication.java
@@ -0,0 +1,13 @@
+package com.unishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnishopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnishopApplication.class, args);
+    }
+
+}
